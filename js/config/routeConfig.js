2175194fef42204d261fab4a9fe5b9a45320e40f
divaprates <divasilvaprates@gmail.com>
2025-06-22 22:51:35 -0300

    feat: rotas - novo contato

--- js/config/routeConfig.js
+++ js/config/routeConfig.js
@@ -1,6 +1,13 @@
 angular.module("listaTelefonica").config(function ($routeProvider) {
-    $routeProvider.when("/contatos", {
-        templateUrl: "view/contatos.html",
-        controller: "listaTelefonicaCtrl"
+	$routeProvider.when("/contatos", {
+		templateUrl: "view/contatos.html",
+		controller: "listaTelefonicaCtrl"
     });
+
+	$routeProvider.when("/novoContato", {
+		templateUrl: "view/novoContato.html",
+		controller: "listaTelefonicaCtrl"
+    });
+    
+    $routeProvider.otherwise({redirectTo: "/contatos"});
 });
